d0c2fd9f35215452235fedaf3b79fde99f8ce056
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-04-05 19:20:20 +0600

    first

---
+++ blog/tests.py
@@ -0,0 +1,22 @@
+from django.test import TestCase,Client
+from django.urls import reverse
+# Create your tests here.
+class HelloViewTestCase(TestCase):
+    def setUp(self):
+        self.client = Client()
+    
+    def test_hello(self):
+        response = self.client.get(reverse("hello-view"))
+        expected_data = "Hello"
+        self.assertEqual(expected_data, response.content.decode())
+        self.assertEqual(500, response.status_code)
+        self.assertEqual(response['name'], 'alex')        
+    def test_contacts(self):
+        response = self.client.get(reverse("contacts-view"))
+        expected_data = "number"
+        self.assertEqual(expected_data, response.content.decode())
+
+    def test_about(self):
+        response = self.client.get(reverse("about-view"))
+        expected_data = "about"
+        self.assertEqual(expected_data, response.content.decode())
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+# from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here. from django.http import HttpResponse
+# Create your views here.
+def get_hello(request):
+    return  HttpResponse("Hello", headers={"name": "alex"}, status=500)
+
+def get_contacts(request):
+    return HttpResponse("number")
+
+def get_about(request):
+    return HttpResponse("about")
