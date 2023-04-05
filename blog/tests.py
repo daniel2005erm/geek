dee639af2d4074a00becbacccf2ed5ec3ea4f5b7
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-04-05 19:24:12 +0600

    dz2

--- blog/tests.py
+++ blog/tests.py
@@ -10,7 +10,8 @@ class HelloViewTestCase(TestCase):
         expected_data = "Hello"
         self.assertEqual(expected_data, response.content.decode())
         self.assertEqual(500, response.status_code)
-        self.assertEqual(response['name'], 'alex')        
+        self.assertEqual(response['name'], 'alex')    
+            
     def test_contacts(self):
         response = self.client.get(reverse("contacts-view"))
         expected_data = "number"
